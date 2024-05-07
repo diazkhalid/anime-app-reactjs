22c2ff029cd10b85d98e6286dad7ddc57df7710e
diazka_ <diazananda02@gmail.com>
2024-05-08 03:54:41 +0800

    seperate App into smaller components

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react';
+import Logo from './Logo';
+import Search from './Search';
+
+export default function NavBar({ animes }) {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search animes={animes} />
+    </nav>
+  );
+}
